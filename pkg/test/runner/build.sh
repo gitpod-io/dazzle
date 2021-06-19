87d0841bd04ac7e00fdf3fb10d1c1923a7d8123d
rl-gitpod <robert@gitpod.io>
2021-06-19 04:27:05 +0000

    Run buildkit & local registry in gh action Build test runner in gh action Make build.sh a little more robust

--- pkg/test/runner/build.sh
+++ pkg/test/runner/build.sh
@@ -5,8 +5,17 @@ GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o bin/runner_linux_amd64 main.go
 curl -L https://github.com/upx/upx/releases/download/v3.96/upx-3.96-amd64_linux.tar.xz | tar xJ
 upx-3.96-amd64_linux/upx bin/runner_linux_amd64 
 rm -r upx-3.96-amd64_linux
-go get github.com/GeertJohan/go.rice/rice
-rice embed-go -i github.com/csweichel/dazzle/pkg/test/runner
+go install github.com/GeertJohan/go.rice/rice@v1.0.2
+RICEBIN="$GOBIN"
+if [ -z "$RICEBIN" ]; then
+  if [ -z "$GOPATH" ]; then
+    RICEBIN="$HOME"/go/bin
+  else
+    RICEBIN="$GOPATH"/bin
+  fi
+fi
+
+"$RICEBIN"/rice embed-go -i github.com/csweichel/dazzle/pkg/test/runner
 
 if [ $(ls -l bin/runner_linux_amd64 | cut -d ' ' -f 5) -gt 3437900 ]; then
     echo "runner binary is too big (> gRPC message size)"
